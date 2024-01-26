556edd293ac741ab6cddd1a1681f76089e8debd0
K0nstant1nS <K1kollov@gmail.com>
2024-01-26 20:23:31 +0300

    feat: pages

--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from "../services/store";
-
-export function checkResponse(res:Response):Promise<any> {
-  return res.ok
-    ? res.json()
-    : Promise.reject(new Error("Ошибка при попытке получить данные"));
-}
-
-export const getPostsS = (s: RootState) => s.posts;
